b72eecb7ef3b14c692325cab5cb6277e53d23c40
Abhishek kumar jha <Abhishekkrjha34@gmail.com>
2025-06-18 16:36:47 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Nabvar from './components/shared/Nabvar'
 function App() {
 
   return (
-    <>
+    <div className='bg-[#F6F8FC] h-screen w-screen overflow-hidden'>
       <Nabvar />
-    </>
+    </div>
   )
 }
 
--- src/components/shared/Nabvar.jsx
+++ src/components/shared/Nabvar.jsx
@@ -1,20 +1,51 @@
 import React from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
-const Nabvar = () => {
+import { IoIosSearch } from "react-icons/io";
+import { BiSupport } from "react-icons/bi";
+import { MdSettings } from "react-icons/md";
+import { PiDotsNine } from "react-icons/pi";
+import Avatar from '@mui/material/Avatar';
+
+const Navbar = () => {
   return (
     <div className="flex items-center justify-between mx-3 h-16">
       <div className="flex items-center gap-10">
         <div className="flex items-center gap-2">
           <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
-            <RxHamburgerMenu className="text-2xl text-gray-600" />
+            <RxHamburgerMenu size={"28px"}/>
           </div>
-          <img className="w-8" src="https://images.app.goo.gl/YMD4hbi7kQRrY8uk7" />
-
-
+          <img className="w-8" src="src/assets/email_logo.jpg" alt="Email Logo"/>
+          <h1 className="text-2xl text-gray-500 font-medium">Email</h1>
+        </div>
+      </div>
+      <div className="md:block hidden w-[50%] mr-60">
+        <div className="flex items-center px-2 py-3 rounded-full bg-blue-100">
+          <IoIosSearch size={"24px"} className="text-gray-500"/>
+          <input
+            type="text"
+            placeholder="Search Mail"
+            className="w-full px-1 bg-transparent outline-none rounded-full"
+          />
+        </div>
+      </div>
+      <div className="md:block hidden">
+        <div className="flex items-center gap-2">
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <BiSupport size={"28px"} className="text-gray-500"/>
+          </div>
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <MdSettings size={"28px"} className="text-gray-500"/>
+          </div>
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <PiDotsNine size={"28px"} className="text-gray-500"/>
+          </div>
+          <div className="cursor-pointer" >
+            <Avatar alt="Abhishek Kumar Jha" src="src/assets/userimg.png" />
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Nabvar;
+export default Navbar;
